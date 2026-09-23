4544050cc12ee895d6b3ee98b11ae687eab1fb23
小未迪迪 <2689678819@qq.com>
2026-09-23 10:08:50 +0800

    chore:移除view文件夹提交给队友

--- view/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:view/routes/index.dart';
-
-void main() {
-  // runApp启动应用，根路由为 getRootWidget 函数返回的 MaterialApp
-  runApp(getRootWidget());
-}
